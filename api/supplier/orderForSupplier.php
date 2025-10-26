5b479bf45bb97e945b5a965f23360f6892e25d07
welshyohans <welsh123yohans@gmail.com>
2025-10-27 02:48:35 +0300

    small edit

--- api/supplier/orderForSupplier.php
+++ api/supplier/orderForSupplier.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: access");
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 header('Content-Type: application/json; charset=UTF-8');
