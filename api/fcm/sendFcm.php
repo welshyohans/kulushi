f2a73632e4a18954e12e1bad399b7298dbc36b0a
welshyohans <welsh123yohans@gmail.com>
2025-10-27 04:58:43 +0300

    about fcm

---
+++ api/fcm/sendFcm.php
@@ -0,0 +1,27 @@
+<?php
+
+
+include_once '../../config/Database.php';
+include_once '../../model/FCM.php';
+
+
+
+//$body = $data['message'];
+$fcmCode = "eXV_d2fzR8OBcRfz6x3KTj:APA91bFDSK2p1uIKzp6gFNpR_v47TGOsfX2_IofHjY7hjigi-qtWYikPD9mCLTK5jDeBGKyv8Nr_fTgnFv_uMj5jMy3ZLq7YLXNuJsN9JVjcT7A19F97e_0";
+$title = "me i am title";
+//$isNotifable = $data['is_notifable'];
+//$goodsIdList = $data['goodsIdList'];
+
+
+
+
+    
+  $fcm = new FCMService('from-merkato', '../../model/mp.json');
+  //$notificationDetail = '1$0&'.$title.'&'.$body.'&no|'.$goodsIdList;
+  $result = $fcm->sendFCM($fcmCode, $title, "notification body", 0, "notification detail");
+
+echo $result;
+
+
+
+?>
