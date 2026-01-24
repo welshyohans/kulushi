dcae75858187e0e50bee6817dbc32993eb79c18a
welshyohans <welsh123yohans@gmail.com>
2026-01-24 07:28:19 +0300

    updating for cross website

--- api/address/getAllAddress.php
+++ api/address/getAllAddress.php
@@ -1,11 +1,20 @@
 <?php
+// CORS headers - allow cross-origin requests
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json');
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 include_once '../../config/Database.php';
 include_once '../../model/Address.php';
 
-
 $database = new Database();
-
 $db = $database->connect();
 
 $address = new Address($db);
